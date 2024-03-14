6e3e2b50192d948c685da72d70c93fa175536375
sgtpjbarker <P47Thunderbolt!>
2024-03-14 10:25:45 -0700

    Fixes

--- CombatResults.java
+++ CombatResults.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.Random;
 public class CombatResults {
     int wounds=0;
     int rake=0;
@@ -11,6 +12,7 @@ public class CombatResults {
     int overKill=0;
     static int attackerResult=0;
     static int defenderResult=0;
+    static int roll=0;
     public static int attackerResults(){
         Scanner attacker = new Scanner(System.in);
         System.out.println("Attacker, please ask these questions.");
@@ -39,7 +41,7 @@ public class CombatResults {
         System.out.println("Defender, please ask these questions.");
         System.out.println("How many wounds did you do to the unit?");
         int wounds = attacker.nextInt();
-        System.out.println("How many rakes do you have?");
+        System.out.println("How many ranks do you have?");
         int rake = attacker.nextInt();
         System.out.println("Do you have a standard, enter 1 or 0?");
         int standard = attacker.nextInt();
@@ -47,7 +49,7 @@ public class CombatResults {
         int battleStandard = attacker.nextInt();
         System.out.println("Did you attack the flank, 1 for yes, 0 for no?");
         int flankAttack = attacker.nextInt();
-        System.out.println("Did you attack the rear, 2 for yes, 0 for no?");
+        System.out.println("Did you attack the rear, 1 for yes, 0 for no?");
         int rearAttack = attacker.nextInt();
         System.out.println("Are you on the high ground, 1 for yes, 0 for no?");
         int highGround = attacker.nextInt();
@@ -57,6 +59,10 @@ public class CombatResults {
         return defenderResult=wounds+rake+standard+battleStandard+flankAttack+rearAttack+highGround+overKill;
 
     }
+        public int rollDice(){
+            System.out.println("Here is a dice roll");
+            return roll;
+        }
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
